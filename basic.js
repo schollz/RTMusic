4cb7807656d3702975a5cfe1c8c151de0f525ca2
Eric Douglas <eric.douglas@tufts.edu>
2013-11-15 22:15:56 -0500

    added webaudioapi html file

--- basic.js
+++ basic.js
@@ -2,14 +2,14 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-app.listen(80);
+app.listen(82);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
+  fs.readFile(__dirname + '/webaudioapi.html',
   function (err, data) {
     if (err) {
       res.writeHead(500);
-      return res.end('Error loading index.html');
+      return res.end('Error loading webaudioapi.html');
     }
 
     res.writeHead(200);
@@ -18,9 +18,15 @@ function handler (req, res) {
 }
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+  //socket.emit('news', { hello: 'world' });
   socket.on('userData', function (data) {
     io.sockets.emit('update', data);
+  });
+  socket.on('play', function (data) {
+    io.sockets.emit('play', data);
+  });
+  socket.on('pause', function (data) {
+    io.sockets.emit('pause', data);
   });
   // setInterval(function(){
   //   socket.emit('update', {hello: "Hello"});
