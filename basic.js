b016241dea976be1b898848784e3ec246e0585b9
Jasper Degens <jasperdegens@gmail.com>
2013-11-19 19:18:07 -0500

    my stuff

--- basic.js
+++ basic.js
@@ -1,8 +1,10 @@
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
+  , dgram = require('dgram')
 
 app.listen(80);
+
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
@@ -16,6 +18,17 @@ function handler (req, res) {
     res.end(data);
   });
 }
+
+var udp = dgram.createSocket("udp4", function(){
+  console.log('message');
+});
+
+var message = new Buffer("Some bytes");
+setInterval(function(){
+  udp.send(message, 0, message.length, 41234, "localhost", function(err, bytes) {
+    console.log(bytes);
+  });
+}, 5000);
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
