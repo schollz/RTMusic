3905993c720782a89c16f8a69f9c1dae7ed5a2e9
Jasper Degens <jasperdegens@gmail.com>
2013-11-21 00:25:52 -0500

    fixed merge conflicts

--- audio.js
+++ audio.js
@@ -13,29 +13,21 @@ function handler (req, res) {
 var app = require('http').createServer(handler);
 var fs = require('fs');
 
-<<<<<<< HEAD
 var BinaryServer = require('binaryjs').BinaryServer;
 
 
-=======
->>>>>>> 8eb8ab6cb5bbe116e77145a89f1b4e377994f62c
 app.listen(9000);
 var io = require('socket.io').listen(5000);
 
 
 
 var bs = new BinaryServer({server: app, path: '/binary-endpoint'});
-<<<<<<< HEAD
 // Wait for new user connections
 bs.on('connection', function(client){
   // Stream a flower as a hello!
   console.log('binary ready');
   client.on('stream', function(stream, meta){
     // broadcast to all other clients
-=======
-bs.on('connection', function(client) {
-  client.on('stream', function(stream, meta) {
->>>>>>> 8eb8ab6cb5bbe116e77145a89f1b4e377994f62c
     console.log('client activity');
     for (var id in bs.clients) {
       if (bs.clients.hasOwnProperty(id)) {
