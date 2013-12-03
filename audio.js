c624e48a4f20daf68a6b9ca480f3aef042ea72ff
Jasper Degens <jasperdegens@gmail.com>
2013-12-03 18:11:07 -0500

    binary added

--- audio.js
+++ audio.js
@@ -13,7 +13,7 @@ function handler (req, res) {
 var app = require('http').createServer(handler);
 var fs = require('fs');
 
-// var BinaryServer = require('binaryjs').BinaryServer;
+var BinaryServer = require('binaryjs').BinaryServer;
 
 var port = process.env.PORT || 5000;
 app.listen(port);
@@ -21,25 +21,25 @@ app.listen(port);
 // console.log("server is good to go");
 
 
-// var bs = new BinaryServer({server: app, path: '/binary-endpoint'});
-// // Wait for new user connections
-// bs.on('connection', function(client){
-//   // Stream a flower as a hello!
-//   // console.log('binary ready');
-//   client.on('stream', function(stream, meta){
-//     // broadcast to all other clients
-//     // console.log('client activity');
-//     for (var id in bs.clients) {
-//       if (bs.clients.hasOwnProperty(id)) {
-//         var otherClient = bs.clients[id];
-//         if (otherClient != client) {
-//           var send = otherClient.createStream(meta);
-//           stream.pipe(send);
-//         }
-//       }
-//     }
-//   });
-// });
+var bs = new BinaryServer({server: app, path: '/binary-endpoint'});
+// Wait for new user connections
+bs.on('connection', function(client){
+  // Stream a flower as a hello!
+  // console.log('binary ready');
+  client.on('stream', function(stream, meta){
+    // broadcast to all other clients
+    // console.log('client activity');
+    for (var id in bs.clients) {
+      if (bs.clients.hasOwnProperty(id)) {
+        var otherClient = bs.clients[id];
+        if (otherClient != client) {
+          var send = otherClient.createStream(meta);
+          stream.pipe(send);
+        }
+      }
+    }
+  });
+});
 
 // io.sockets.on('connection', function (socket) {
 //   socket.on('play', function (data) {
