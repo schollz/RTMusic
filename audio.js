0cac8099d8b61c067b3e4c91a6683b4b804bc636
Jasper Degens <jasperdegens@gmail.com>
2013-12-06 20:29:03 -0500

    fixed routing :

--- audio.js
+++ audio.js
@@ -1,17 +1,24 @@
 function handler (req, res) {
-  fs.readFile(__dirname + '/audio.html',
+  
+  var path = url.parse(req.url).pathname;
+  if(path == '/'){
+    path = '/audio.html';
+  }
+  fs.readFile(__dirname + path,
   function (err, data) {
     if (err) {
       res.writeHead(500);
       return res.end('Error loading audio.html');
     }
     res.writeHead(200);
-    res.end(data);
+    res.write(data);
+    res.end();
   });
 }
 
 var app = require('http').createServer(handler);
 var fs = require('fs');
+var url = require('url');
 
 var BinaryServer = require('binaryjs').BinaryServer;
 
